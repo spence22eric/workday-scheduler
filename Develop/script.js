d7cad6bbd8d2c54222eaa36e29ea20f77a89061f
Eric Spencer <spencer22eric@gmail.com>
2022-03-11 14:11:29 -1000

    add time-sensitive classes

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+let textInput = $("#text-input");
+
+let now = new Date($.now());
+let currentDate = moment(now).format('dddd MMM Do');
+$("#currentDay").html(currentDate);
+
+let currentHour = new Date(now).getHours()
+console.log(currentHour);
+$(document).ready(function () {
+
+    for (let i = 0; i < 9; i++) {
+        let id = "#hour-" + i;
+        let hour = 9 + i;
+
+        if (hour < currentHour) {
+            $(id).addClass("past");
+        }
+        else if (hour === currentHour) {
+            $(id).addClass("present");
+        }
+        else {
+            $(id).addClass("future");
+        }
+    }
+});
